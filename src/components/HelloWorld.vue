cafe302db12c49ad182ea8de803cdd20e052fdd5
tetsu5555 <tetete1118@gmail.com>
2019-03-06 00:09:42 +0900

    import data from data.json, desplay it

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
     <h1>Welcome to the Forum</h1>
+    <div v-for="thread in threads" :key="thread.Id">
+      <h2>{{thread.title}}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+        <p>{{users[posts[postId].userId].name}}</p>
+        <p>{{posts[postId].text}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import sourceData from '@/data'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
     }
   }
 }
